88779ab4cafaf36a2fbc0f956356198a1f056ef8
Phan Văn Thanh <phanvanthanhda10tt@gmail.com>
2015-08-04 09:55:00 +0700

    add edit button

--- template/js/custom.js
+++ template/js/custom.js
@@ -1,54 +1,40 @@
 
 $(document).ready(function(){
-    /*Table responsive*/
-    /*$('row-danh-sach-giang-vien').affix({
-      offset: {
-        top: 50
-      }
-    });*/
+    
+    stickyDanhSachGiangVien();
     tableResponsive();
-    /*filter danh sach giang vien*/
-    $('.filterable .btn-filter').click(function(){
-        var $panel = $(this).parents('.filterable'),
-        $filters = $panel.find('.filters input'),
-        $tbody = $panel.find('.table tbody');
-        if ($filters.prop('disabled') == true) {
-            $filters.prop('disabled', false);
-            $filters.first().focus();
-        } else {
-            $filters.val('').prop('disabled', true);
-            $tbody.find('.no-result').remove();
-            $tbody.find('tr').show();
-        }
-    });
 
-    $('.filterable .filters input').keyup(function(e){
-        /* Ignore tab key */
-        var code = e.keyCode || e.which;
-        if (code == '9') return;
-        /* Useful DOM data and selectors */
-        var $input = $(this),
-        inputContent = $input.val().toLowerCase(),
-        $panel = $input.parents('.filterable'),
-        column = $panel.find('.filters th').index($input.parents('th')),
-        $table = $panel.find('.table'),
-        $rows = $table.find('tbody tr');
-        /* Dirtiest filter function ever ;) */
-        var $filteredRows = $rows.filter(function(){
-            var value = $(this).find('td').eq(column).text().toLowerCase();
-            return value.indexOf(inputContent) === -1;
-        });
-        /* Clean previous no-result if exist */
-        $table.find('tbody .no-result').remove();
-        /* Show all rows, hide filtered ones (never do that outside of a demo ! xD) */
-        $rows.show();
-        $filteredRows.hide();
-        /* Prepend no-result row if all rows are filtered */
-        if ($filteredRows.length === $rows.length) {
-            $table.find('tbody').prepend($('<tr class="no-result text-center"><td colspan="'+ $table.find('.filters th').length +'">No result found</td></tr>'));
+    // attach table filter plugin to inputs
+    $('[data-action="filter"]').filterTable();    
+    $('.danh-sach-giang-vien').on('click', '.panel-heading span.filter', function(e){
+        var $this = $(this), 
+            $panel = $this.parents('.panel');
+        
+        $panel.find('.panel-body').slideToggle();
+        if($this.css('display') != 'none') {
+            $panel.find('.panel-body input').focus();
         }
     });
+    $('[data-toggle="tooltip"]').tooltip();
+
 });
+
+function stickyDanhSachGiangVien(){
+    $(window).resize(function(){
+        if ($(window).width()<=750) {            
+            $(".danh-sach-giang-vien").unstick();
+        }
+        else{            
+            $(".danh-sach-giang-vien").sticky({topSpacing:80});
+        }
+    });
+    if ($(window).width()<=750) {
+        $(".danh-sach-giang-vien").unstick();        
+    }
+    else{        
+        $(".danh-sach-giang-vien").sticky({topSpacing:80});
+    }
+}
 
 function tableResponsive(){
     $(window).resize(function(){
@@ -56,14 +42,48 @@ function tableResponsive(){
             jQuery('table.cf tbody tr td:first-child').addClass('active');            
         }
         else{
-            jQuery('table.cf body tr td:first-child').removeClass('active');
+            jQuery('table.cf tbody tr td:first-child').removeClass('active');
         }
     });
     if ($(window).width()<=767) {
         jQuery('table.cf tbody tr td:first-child').addClass('active');
         
     }
-    else{
-        jQuery('table.cf body tr td:first-child').removeClass('active');
+    else{        
+        jQuery('table.cf body tr td:first-child').removeClass('active');     
     }
 }
+
+(function(){
+    'use strict';
+    var $ = jQuery;
+    $.fn.extend({
+        filterTable: function(){
+            return this.each(function(){
+                $(this).on('keyup', function(e){
+                    $('.filterTable_no_results').remove();
+                    var $this = $(this), 
+                        search = $this.val().toLowerCase(), 
+                        target = $this.attr('data-filters'), 
+                        $target = $(target), 
+                        $rows = $target.find('tbody tr');
+                        
+                    if(search == '') {
+                        $rows.show(); 
+                    } else {
+                        $rows.each(function(){
+                            var $this = $(this);
+                            $this.text().toLowerCase().indexOf(search) === -1 ? $this.hide() : $this.show();
+                        })
+                        if($target.find('tbody tr:visible').size() === 0) {
+                            var col_count = $target.find('tr').first().find('td').size();
+                            var no_results = $('<tr class="filterTable_no_results"><td colspan="'+col_count+'">No results found</td></tr>')
+                            $target.find('tbody').append(no_results);
+                        }
+                    }
+                });
+            });
+        }
+    });
+    $('[data-action="filter"]').filterTable();
+})(jQuery);
